99197cfec2a08aaf3f8927b9b615dd85c4121b48
jerabelo <70666626+jerabelo@users.noreply.github.com>
2022-02-15 20:00:29 -0700

    Created methods to check for ps1 & to execute

--- my_shell/shell.py
+++ my_shell/shell.py
@@ -1,30 +1,41 @@
-import os,re,sys
+import os, sys, re
 
-path = os.getcwd()
-dir_list = os.listdir(path)
-
-def CurrPath():
-    os.write(path)
-
-def ChangePath(args):
+#Method for checking ps1 variable and returning current shell ps1
+def check_prompt():
     try:
-        os.chdir(args)
-    except FileNotFoundError:
-        os.write(f'file{args[1]} not found!\n'.encode())
+        sys.ps1
+    except AttributeError:
+        sys.ps1 = "$ "
+        return sys.ps1
 
-def main(*args):
-    while(True):
-        os.fork()
-        if len(args) == 0:
-            os.write('$ ')
-            prompt = input()
-            if prompt == 'ls':
-                CurrPath()
-            elif prompt == 'dir':
-                os.write('File & directories: ')
-                os.write(dir_list)
-                continue
+#Method for taking in commands and executing them
+def execute_commands():
+    commands = input(f'{sys.ps1}').split(" ")
+    pid = os.getpid()
+    os.write(2, ("Fork with PID: %d\n" % pid).encode())
+    child = os.fork()
+    if child < 0:
+        os.write(2,("error: Failed to fork with PID: %d\n" % pid).encode())
+        sys.exit(1)
+    elif child == 0:
+        if commands == 'cd':
+            os.write(2, "cd command executing...\n".encode())
+        else:
+            for directory in re.split(":", os.enviorn['PATH']):
+                prog = "%s/%s" % (directory, commands[0])
+                try:
+                    os.execve(prog, commands, os.environ)
+                except FileNotFoundError:
+                    pass # fail quietly
+                os.write(1,("%s command not found.\n" % commands[0]).encode())
+                sys.exit(1)
+            else:
+                child_pid_code = os.wait()
 
+def main():
+    while True:
+        check_prompt()
+        execute_commands()
 
-
+main()
 
