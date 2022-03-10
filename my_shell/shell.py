f6fdb0d30ba517661574cdc9746c5dcb285a0879
jerabelo <70666626+jerabelo@users.noreply.github.com>
2022-03-10 12:12:06 -0700

    Functional shell, working on archiver

--- my_shell/shell.py
+++ my_shell/shell.py
@@ -11,32 +11,34 @@ def token():
         #set os.environ and then return
         return os.environ['PS1']
 
+#method for displaying core commands of a shell
 def menu(commands):
     if commands[0] == "exit":
         sys.exit(1)
     elif commands[0] == "cd":
         cd_command(commands)
     elif commands.__contains__("|"):
-        commands = pipe_commands(commands)
-        (r,w) = os.pipe()
-        os.set_inheritable(0,True)
-        os.set_inheritable(1,True)
-        #closing output
-        os.close(0)
-        os.dup(r)
-        execute(commands)
+        run_pipe(commands)
+    elif commands.__contains__('tar'):
+        archive(commands)
+#creating child using fork
     child = os.fork()
+    #if no child exists, exit
     if child < 0:
         sys.exit(1)
-    elif child == 0:
+    elif child == 0: #child 1 executing
+        #redirection for writing into file
         if commands.__contains__(">"):
+            #closing standard fd for writting in
             os.close(1)
+            #open 2nd command,create file if it doesn't exist & open for writing
             os.open(commands[2], os.O_CREAT | os.O_WRONLY)
             #making child writing fd inheritable
             os.set_inheritable(1, True)
             commands = commands[:1]
-
+        #redirection for displaying contents out
         elif commands.__contains__("<"):
+            #closing standard fd for reading
             os.close(0)
             os.open(commands[2], os.O_RDONLY)
             # making child reading fd inheritable
@@ -46,7 +48,9 @@ def menu(commands):
     else:
         childPidCode = os.wait()
 
+#method for changing path based on path
 def cd_command(path):
+    print(path)
     if len(path) > 1:
         try:
             os.chdir(path[1])
@@ -63,6 +67,7 @@ def main():
         menu(commands)
         continue
 
+#method for checking all directories & splitting to check viable path
 def execute(commands):
     for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
         program = "%s/%s" % (dir, commands[0])
@@ -73,11 +78,61 @@ def execute(commands):
     os.write(2, ("Child: Error: Could not exec %s\n" % commands[0]).encode())
     sys.exit(1)
 
-def pipe_commands(commands):
-    for line in commands:
-        cmds = line.split('|')
-        return cmds
+#pipe method
+def run_pipe(commands):
+    (r, w) = os.pipe()
+    for rw in (r,w):
+        os.set_inheritable(r,True)
+    p1 = os.fork()
+    # only needs to write at the write end
+    if p1 < 0:
+        os.write(1,'Filed to forked.'.encode())
+        sys.exit(1)
+    elif p1 == 0: #closing the standard fd for reading (r)
+        os.close(1) #close writing fd
+        #duplicate writing fd
+        os.dup(w)
+        os.set_inheritable(1,True)
+        for rw in (r,w):
+            os.close(rw)
+        commands = commands[0:commands.index("|")] #get command up to pipe symbol
+        #execute commands
+        execute(commands)
+        sys.exit(1)
+    else:
+        #create 2nd fork
+        p2 = os.fork()
+        if p2 < 0:
+            os.write(1,'Failed to fork second child'.encode())
+            sys.exit(1)
+            #if p2 has has been foked > 0
+        elif p2 == 0:
+            # close standard reading fd
+            os.close(0)
+            #duplicate reading fd
+            os.dup(r)
+            os.set_inheritable(0,True)
+            for rw in (r,w):
+                os.close(rw)
+                # get commmand after pipe symbol
+            commands = commands[commands.index("|") + 1:]
+            execute(commands)
+            sys.exit(1)
+        else:
+            childPidCode = os.wait()
+            for rw in (r,w):
+                os.close(rw)
+            childPidCode = os.wait()
 
+#working on archiving method
+#design when sender send archieve file, find typ eof file, what bytes belong to first file, and second
+#encode info about length of file and file name, everything else if extra, you can add
+#extract original files from archive
+def archive(commands):
+    file = commands[1]
+    path = os.getcwd()
+    full_path = file + "/" + path
+    execute(full_path)
 
 if '__main__' == __name__:
     main()
